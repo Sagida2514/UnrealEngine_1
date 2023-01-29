cafa41d3f9d8d14fc6991052add8b7ac660c81f6
sagida2514 <90020964+Sagida2514@users.noreply.github.com>
2023-01-29 16:41:19 +0900

    Pus

---
+++ TestUnrealProject/Intermediate/Build/Win64/TestUnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30146
+#include "C:/Workspace/TestUnrealProject/Intermediate/Build/Win64/TestUnrealProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ TestUnrealProject/Intermediate/Build/Win64/UE4Editor/Inc/TestUnrealProject/TestUnrealProjectGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TESTUNREALPROJECT_TestUnrealProjectGameModeBase_generated_h
+#error "TestUnrealProjectGameModeBase.generated.h already included, missing '#pragma once' in TestUnrealProjectGameModeBase.h"
+#endif
+#define TESTUNREALPROJECT_TestUnrealProjectGameModeBase_generated_h
+
+#define TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_SPARSE_DATA
+#define TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_RPC_WRAPPERS
+#define TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATestUnrealProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_ATestUnrealProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATestUnrealProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/TestUnrealProject"), NO_API) \
+	DECLARE_SERIALIZER(ATestUnrealProjectGameModeBase)
+
+
+#define TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesATestUnrealProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_ATestUnrealProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATestUnrealProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/TestUnrealProject"), NO_API) \
+	DECLARE_SERIALIZER(ATestUnrealProjectGameModeBase)
+
+
+#define TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATestUnrealProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATestUnrealProjectGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATestUnrealProjectGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATestUnrealProjectGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATestUnrealProjectGameModeBase(ATestUnrealProjectGameModeBase&&); \
+	NO_API ATestUnrealProjectGameModeBase(const ATestUnrealProjectGameModeBase&); \
+public:
+
+
+#define TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATestUnrealProjectGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATestUnrealProjectGameModeBase(ATestUnrealProjectGameModeBase&&); \
+	NO_API ATestUnrealProjectGameModeBase(const ATestUnrealProjectGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATestUnrealProjectGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATestUnrealProjectGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATestUnrealProjectGameModeBase)
+
+
+#define TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_12_PROLOG
+#define TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_SPARSE_DATA \
+	TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_RPC_WRAPPERS \
+	TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_INCLASS \
+	TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_SPARSE_DATA \
+	TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TESTUNREALPROJECT_API UClass* StaticClass<class ATestUnrealProjectGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TestUnrealProject_Source_TestUnrealProject_TestUnrealProjectGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
